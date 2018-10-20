f9a5c56beb23f7dd93696b66696e64507a835aaa
khanhnguyen <khanhnguyentitidj@gmail.com>
2018-10-20 08:57:53 +0700

    add dead method cat

--- cat.js
+++ cat.js
@@ -2,6 +2,12 @@ function cat(name,age) {
 	// body...
 	this.name = name;
 	this.age = age;
-}
+	this.dead;
+};
+
+cat.prototype.dead = function() {
+	// body...
+	this.dead = true;
+};
 
 module.exports = cat;
